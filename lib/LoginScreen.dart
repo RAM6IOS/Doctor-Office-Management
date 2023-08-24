7744c8d045af659c5add7da58724abbe88b0230c
RAM6IOS <ios191169@gmail.com>
2023-08-24 22:53:43 +0100

    first commit

---
+++ lib/LoginScreen.dart
@@ -0,0 +1,207 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Doctor Login')),
+      body: LoginForm(),
+    );
+  }
+}
+
+class LoginForm extends StatefulWidget {
+  @override
+  _LoginFormState createState() => _LoginFormState();
+}
+
+class _LoginFormState extends State<LoginForm> {
+  final _formKey = GlobalKey<FormState>();
+  String _email = '';
+  String _password = '';
+
+  void _login() {
+    if (_formKey.currentState?.validate() == true) {
+      // Perform authentication here
+      // You can use Firebase Authentication or any other method
+
+      // For demonstration purposes, let's assume successful login
+      Navigator.pushReplacementNamed(context, '/dashboard'); // Replace with your dashboard route
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: _formKey,
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            TextFormField(
+              keyboardType: TextInputType.emailAddress,
+              decoration: InputDecoration(labelText: 'Email'),
+              validator: (value) {
+                if (value?.isEmpty == true) {
+                  return 'Please enter your email';
+                }
+                return null;
+              },
+              onChanged: (value) {
+                setState(() {
+                  _email = value;
+                });
+              },
+            ),
+            TextFormField(
+              obscureText: true,
+              decoration: InputDecoration(labelText: 'Password'),
+              validator: (value) {
+                if (value?.isEmpty == true) {
+                  return 'Please enter your password';
+                }
+                return null;
+              },
+              onChanged: (value) {
+                setState(() {
+                  _password = value;
+                });
+              },
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: _login,
+              child: Text('Login'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+
+
+// doctor_registration_screen.dart
+
+
+
+
+class DoctorAuthenticationScreen extends StatefulWidget {
+  @override
+  _DoctorAuthenticationScreenState createState() => _DoctorAuthenticationScreenState();
+}
+
+class _DoctorAuthenticationScreenState extends State<DoctorAuthenticationScreen> {
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Doctor Authentication')),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            TextFormField(
+              controller: emailController,
+              decoration: InputDecoration(labelText: 'Email'),
+            ),
+            SizedBox(height: 16),
+            TextFormField(
+              controller: passwordController,
+              decoration: InputDecoration(labelText: 'Password'),
+              obscureText: true,
+            ),
+            SizedBox(height: 32),
+            ElevatedButton(
+              onPressed: () {
+                // Implement your login logic here
+                // For example: verify email and password
+                // Then navigate to the doctor's dashboard
+                Navigator.push(context, MaterialPageRoute(builder: (context) => DoctorDashboard()));
+              },
+              child: Text('Log In'),
+            ),
+            SizedBox(height: 16),
+            ElevatedButton(
+              onPressed: () {
+                // Navigate to the sign-up screen
+                Navigator.push(context, MaterialPageRoute(builder: (context) => DoctorSignUpScreen()));
+              },
+              child: Text('Sign Up'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class DoctorSignUpScreen extends StatelessWidget {
+  TextEditingController nameController = TextEditingController();
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+  TextEditingController areaController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Doctor Sign Up')),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            TextFormField(
+              controller: nameController,
+              decoration: InputDecoration(labelText: 'Name'),
+            ),
+            SizedBox(height: 16),
+            TextFormField(
+              controller: emailController,
+              decoration: InputDecoration(labelText: 'Email'),
+            ),
+            SizedBox(height: 16),
+            TextFormField(
+              controller: passwordController,
+              decoration: InputDecoration(labelText: 'Password'),
+              obscureText: true,
+            ),
+            SizedBox(height: 16),
+            TextFormField(
+              controller: areaController,
+              decoration: InputDecoration(labelText: 'Area of Expertise'),
+            ),
+            SizedBox(height: 32),
+            ElevatedButton(
+              onPressed: () {
+                // Implement your sign-up logic here
+                // For example: create a new doctor account
+                // Then navigate to the doctor's dashboard
+                Navigator.push(context, MaterialPageRoute(builder: (context) => DoctorDashboard()));
+              },
+              child: Text('Sign Up'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class DoctorDashboard extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Doctor Dashboard')),
+      body: Center(
+        child: Text('Welcome to the Doctor Dashboard'),
+      ),
+    );
+  }
+}
---
+++ lib/doctorregis.dart
@@ -0,0 +1,91 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+//import 'package:provider/provider.dart';
+
+
+
+class Doctor {
+  final String name;
+  final String email;
+  final String password;
+  final String areaOfExpertise;
+
+  Doctor({required this.name, required this.email, required this.password, required this.areaOfExpertise});
+}
+class AuthService {
+  final List<Doctor> _doctors = [];
+
+  bool signUp(String name, String email, String password, String areaOfExpertise) {
+    if (_doctors.any((doctor) => doctor.email == email)) {
+      return false; // Email already exists
+    }
+
+    _doctors.add(Doctor(name: name, email: email, password: password, areaOfExpertise: areaOfExpertise));
+    return true; // Sign-up successful
+  }
+
+  Doctor? login(String email, String password) {
+  final doctor = _doctors.firstWhere(
+    (doctor) => doctor.email == email && doctor.password == password,
+    orElse: () => Doctor(name: '', email: '', password: '', areaOfExpertise: ''),
+  );
+  return doctor;
+}
+
+
+}
+
+class AuthProvider extends ChangeNotifier {
+  final AuthService _authService = AuthService();
+  Doctor? _loggedInDoctor;
+
+  Doctor? get loggedInDoctor => _loggedInDoctor;
+
+  bool signUp(String name, String email, String password, String areaOfExpertise) {
+    final success = _authService.signUp(name, email, password, areaOfExpertise);
+    if (success) {
+      _loggedInDoctor = Doctor(name: name, email: email, password: password, areaOfExpertise: areaOfExpertise);
+      notifyListeners(); // This should work correctly since AuthProvider extends ChangeNotifier
+    }
+    return success;
+  }
+
+  bool login(String email, String password) {
+    final doctor = _authService.login(email, password);
+    if (doctor != null) {
+      _loggedInDoctor = doctor;
+      notifyListeners(); // This should work correctly since AuthProvider extends ChangeNotifier
+    }
+    return doctor != null;
+  }
+
+  void logout() {
+    _loggedInDoctor = null;
+    notifyListeners(); // This should work correctly since AuthProvider extends ChangeNotifier
+  }
+}
+
+class SignUpScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    //final authProvider = Provider.of<AuthProvider>(context, listen: false);
+
+    return Scaffold(
+      appBar: AppBar(title: Text('Doctor Sign Up')),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+           // authProvider.signUp('Doctor Name', 'doctor@example.com', 'password', 'Area of Expertise');
+          },
+          child: Text('Sign Up'),
+        ),
+      ),
+    );
+  }
+}
+
+
+
+
+
+
